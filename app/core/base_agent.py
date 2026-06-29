da9b023d95712aadb501f0e45ee274adc470fa3d
Francisco Macario <franciscomiguelmacario@gmail.com>
2026-06-29 19:46:44 +0100

    Clean legacy duplicated files

--- app/core/base_agent.py
+++
@@ -1,8 +0,0 @@
-class BaseAgent:
-
-    def __init__(self, name, memory, logger, bus, brain=None):
-        self.name = name
-        self.memory = memory
-        self.logger = logger
-        self.bus = bus
-        self.brain = brain
